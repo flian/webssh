6ba179dfb5c6e53ee0e702281cecc20bfc9b05cf
foy lian <270263648@qq.com>
2024-02-27 14:59:05 +0800

    page debug for standalone mode.

--- vue2-web/src/main/java/org/lotus/carp/webssh/page/WebSshPageController.java
+++ vue2-web/src/main/java/org/lotus/carp/webssh/page/WebSshPageController.java
@@ -1,10 +1,12 @@
 package org.lotus.carp.webssh.page;
 
+import lombok.extern.slf4j.Slf4j;
 import org.lotus.carp.webssh.page.common.WebSshVue2PageConst;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * <h3>webssh</h3>
@@ -16,12 +18,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @ConditionalOnProperty(value = "webssh.vue2.pageController.enable", matchIfMissing = true)
-public class WebSshPageController {
+@Slf4j
+public class WebSshPageController implements InitializingBean {
+
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
+
+    /**
+     * index with prefix parameter
+     */
+    private static String INDEX_STR = "index.html";
 
 
     @GetMapping(WebSshVue2PageConst.WEB_SSH_VUE2_INDEX)
     public String index() {
-        return String.format("/%s/%s", WebSshVue2PageConst.WEB_SSH_PREFIX, "index.html");
+        return String.format("/%s/%s", WebSshVue2PageConst.WEB_SSH_PREFIX, INDEX_STR);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (null == contextPath || contextPath.isEmpty() || "/".equals(contextPath)) {
+            log.info("not set context path,ignore prefix parameter to front.");
+            return;
+        }
+        INDEX_STR = INDEX_STR + "?prefix=" + contextPath;
     }
 
     /*@GetMapping("/webssh/static/{staticType}/{staticFileFull}")
