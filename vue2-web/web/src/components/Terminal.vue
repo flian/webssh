cd2bddb8b0affdf26baf52cb467b3f335749a92b
foy lian <270263648@qq.com>
2024-02-28 22:14:10 +0800

    remote debug in page

--- vue2-web/web/src/components/Terminal.vue
+++ vue2-web/web/src/components/Terminal.vue
@@ -3,14 +3,7 @@
         <div :id="id">
 
         </div>
-        <div>
-            <el-button
-                id="message_btn"
-                style="position: fixed; margin-left: 94%; margin-top: -24%"
-                class="el-icon-liuyanban"
-                @click="alert('aaa')"
-            ></el-button>
-        </div>
+
     </div>
 </template>
 
