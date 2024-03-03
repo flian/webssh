0354c4afc3fdc5bde46f10b355a47b57baa3401c
foy lian <270263648@qq.com>
2024-03-03 21:43:23 +0800

    prepare release 1.26

--- scripts/docker_build.bat
+++ scripts/docker_build.bat
@@ -8,6 +8,14 @@ mvn clean package -DskipTests
 docker build -f scripts/docker/Dockerfile -t lotus/webssh:${version} .
 docker tag lotus/webssh:${version} lotus/webssh:latest
 
-#push images to docker hub
+
+#may need delete local tag and push again.
+#docker rmi foylian/webssh:${version}
+
+#tag images with hub name
 docker tag lotus/webssh:${version} foylian/webssh:${version}
 docker tag lotus/webssh:${version} foylian/webssh:latest
+
+#push images to docker hub
+docker push foylian/webssh:${version}
+docker push foylian/webssh:latest
