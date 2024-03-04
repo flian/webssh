1567ec1a10ce15c078bc25f3f8128d8a925eb68b
foy lian <270263648@qq.com>
2024-03-04 15:01:24 +0800

    add docker profile.prepare v1.26 release.

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+_sslFile="/app/webssh/websshDockerDefault.p12"
+_sslPwd="websshDocker@Admin123!"
+
+if [ ! -f "$_sslFile" ]; then
+keytool -genkey -alias springboottomcat -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore $_sslFile -validity 3650 -dname "CN=webssh, OU=NoOU, O=NoO, L=Chendu, ST=Sichuan, C=cn" -storepass "$_sslPwd"  -keypass "$_sslPwd"
+fi
 
 sh -c "java -server  ${JAVA_OPTS} -jar ${WEBSSH_JAR_FULL_PATH} ${SPRING_BOOT_OPTS}"
