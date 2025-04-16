3785b6ea78c32a238732df1014dda43e2eef8f0e
foy.lian <ljs2342003@gmail.com>
2025-04-16 13:16:54 +0800

    add enable config for socket and http proxy.

--- webssh-navicat-tunnel/src/main/java/org/lotus/carp/webssh/navicat/config/QuercusConfig.java
+++ webssh-navicat-tunnel/src/main/java/org/lotus/carp/webssh/navicat/config/QuercusConfig.java
@@ -1,10 +1,21 @@
 package org.lotus.carp.webssh.navicat.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.mongo.MongoClientFactory;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <h3>webssh</h3>
@@ -15,6 +26,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  **/
 @Configuration
 @Order(Integer.MIN_VALUE)
+@Slf4j
 public class QuercusConfig {
     @Bean
     public ViewResolver resolver(){
@@ -24,5 +36,20 @@ public class QuercusConfig {
         quercusViewResolver.setViewNames("*.php");
         quercusViewResolver.setOrder(0);
         return quercusViewResolver;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean({MongoClient.class})
+    @ConditionalOnProperty(name = "webssh.init.mongoclient.onstartup",matchIfMissing = true)
+    @Order(value = 123988)
+    public MongoClient mongo(ObjectProvider<MongoClientSettingsBuilderCustomizer> builderCustomizers, MongoClientSettings settings) {
+        MongoClient result = null;
+        //add here to avoid exception on startup when web ssh sever has no mongodb on localhost
+        try{
+            result = (new MongoClientFactory(builderCustomizers.orderedStream().collect(Collectors.toList()))).createMongoClient(settings);
+        }catch (Exception e){
+            log.error("error while create mongoClient.",e);
+        }
+        return result;
     }
 }
